0b8fb93e4a3901d89c1a61a6144baca71fc5c394
Administrator <Administrator@bfyang5130>
2015-11-17 11:12:30 +0800

    增加一个微信支付配置

--- common/config/main.php
+++ common/config/main.php
@@ -1,10 +1,14 @@
 <?php
+
 return [
     'name' => '赚赚乐',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'aliases' => [
+            '@xxkejinet/wxzuanpay' => '@common/extensions/wxzuanpay',
         ],
     ],
 ];
