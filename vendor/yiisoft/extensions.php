b60c1209e32b20eface6c6701065962db889e3ae
Administrator <Administrator@bfyang5130>
2015-11-30 10:17:29 +0800

    增加图片上传组件

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -102,4 +102,22 @@ return array (
       '@bupy7/cropbox' => $vendorDir . '/bupy7/yii2-widget-cropbox',
     ),
   ),
+  '2amigos/yii2-gallery-widget' => 
+  array (
+    'name' => '2amigos/yii2-gallery-widget',
+    'version' => '1.0.1.0',
+    'alias' => 
+    array (
+      '@dosamigos/gallery' => $vendorDir . '/2amigos/yii2-gallery-widget/src',
+    ),
+  ),
+  '2amigos/yii2-file-upload-widget' => 
+  array (
+    'name' => '2amigos/yii2-file-upload-widget',
+    'version' => '1.0.2.0',
+    'alias' => 
+    array (
+      '@dosamigos/fileupload' => $vendorDir . '/2amigos/yii2-file-upload-widget/src',
+    ),
+  ),
 );
