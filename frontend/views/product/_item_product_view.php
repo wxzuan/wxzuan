855401107691467a731ca18b4d6df099ea746a61
Administrator <Administrator@bfyang5130>
2015-12-01 15:52:48 +0800

    购买确定和ue编辑器添加

--- frontend/views/product/_item_product_view.php
+++ frontend/views/product/_item_product_view.php
@@ -1,3 +1,6 @@
+<?php
+use yii\helpers\Url;
+?>
 <div class="decoration"></div>
 <div class="container no-bottom">
     <img class="responsive-image" src="<?= $model->product_s_img ?>" alt="img">
@@ -5,5 +8,5 @@
 <div class="section-title">
     <h4><?= $model->product_name ?><em>￥ <?= $model->product_price ?> 元</em></h4>
     <p style="line-height: 25px;margin-bottom: 10px;"><?= $model->product_description ?></p>
-    <strong><img src="/images/misc/icons/buy.png" width="20" alt="img"></strong>
+    <strong><a onclick ="return(confirm('确认购买吗？'))" href="<?= Url::toRoute('/product/buy/'.$model->product_id) ?>"<img src="/images/misc/icons/buy.png" width="20" alt="img"></a></strong>
 </div>
