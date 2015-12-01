a9ba9eab4e651a8c64d98e7924fe9ed04194166d
Administrator <Administrator@bfyang5130>
2015-12-01 16:01:46 +0800

    购买确定bug

--- frontend/views/product/_item_product_view.php
+++ frontend/views/product/_item_product_view.php
@@ -8,5 +8,5 @@ use yii\helpers\Url;
 <div class="section-title">
     <h4><?= $model->product_name ?><em>￥ <?= $model->product_price ?> 元</em></h4>
     <p style="line-height: 25px;margin-bottom: 10px;"><?= $model->product_description ?></p>
-    <strong><a onclick ="return(confirm('确认购买吗？'))" href="<?= Url::toRoute('/product/buy/'.$model->product_id) ?>"<img src="/images/misc/icons/buy.png" width="20" alt="img"></a></strong>
+    <strong><a onclick ="if(confirm( '确定购买? ')==false) return false; " href="<?= Url::toRoute('/product/buy/'.$model->product_id) ?>"><img src="/images/misc/icons/buy.png" width="20" alt="img"></a></strong>
 </div>
