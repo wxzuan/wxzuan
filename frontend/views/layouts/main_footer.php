e464f8f05aa796620096240d17690088919a931e
Administrator <Administrator@bfyang5130>
2015-12-07 10:48:32 +0800

    通用返回处理

--- frontend/views/layouts/main_footer.php
+++ frontend/views/layouts/main_footer.php
@@ -1,12 +1,18 @@
 <?php
+
 use yii\helpers\Url;
+
+$backurl = \Yii::$app->request->referrer;
+if (strpos($backurl, 'notices')) {
+    $backurl = '/';
+}
 ?>
 <div class="decoration"></div>
 
 <div class="content-footer">
     <p class="copyright-content"> 版权 © 2015 .</p>
     <a href="#" class="go-up-footer"></a>
-    <a href="<?= Url::toRoute('index') ?>" class="facebook-footer"></a>
-    <a href="<?= Url::toRoute('index') ?>" class="twitter-footer"></a>
+    <a href="<?= Url::toRoute('/member/index/index') ?>" class="facebook-footer"></a>
+    <a href="<?= $backurl ?>" class="twitter-footer"></a>
     <div class="clear"></div>
 </div>
