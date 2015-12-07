d07c19ec7a45188ed4b13f6279860390b5b89d45
Administrator <Administrator@bfyang5130>
2015-12-07 10:25:04 +0800

    修改一个布局便于控制

--- frontend/views/layouts/main_footer.php
+++ frontend/views/layouts/main_footer.php
@@ -1,7 +1,9 @@
 <div class="decoration"></div>
 
 <div class="content-footer">
-    <p class="copyright-content"> 版权 © 2015 寻想网络科技 .</p>
+    <p class="copyright-content"> 版权 © 2015 .</p>
     <a href="#" class="go-up-footer"></a>
+    <a href="<?= Url::toRoute('index') ?>" class="facebook-footer"></a>
+    <a href="<?= Url::toRoute('index') ?>" class="twitter-footer"></a>
     <div class="clear"></div>
 </div>
--- frontend/views/layouts/main_header.php
+++ frontend/views/layouts/main_header.php
@@ -3,4 +3,6 @@ use yii\helpers\Url;
 ?>
 <div class="content-header">
     <a href="<?= Url::toRoute('index') ?>" class="content-logo"></a>
+    <a href="<?= Url::toRoute('index') ?>" class="facebook-content"></a>
+    <a href="<?= Url::toRoute('index') ?>" class="twitter-content"></a>
 </div>
