0e8945b742db4ef1eedd56a3bdba23fea13a41fb
Administrator <Administrator@bfyang5130>
2015-12-10 15:03:30 +0800

    增加提现功能

--- frontend/modules/member/views/account/account_tixian.php
+++ frontend/modules/member/views/account/account_tixian.php
@@ -18,12 +18,18 @@ $oneBank = Bankcard::find()->where('user_id=:user_id', [':user_id' => $user_id])
 </div>
 <div class="container no-bottom">
     <a href="#" style="width:100%;padding-left:13px;" class="button-big-icon-colse button-white">所属银行：<span class="float-right"><?= $oneBank->bank_name ?></span></a>
-    <a href="#" style="width:100%;padding-left:13px;" class="button-big-icon-colse button-white">银行帐号：<span class="float-right"><?= '***'.substr($oneBank->account, -4) ?></span></a>
+    <a href="#" style="width:100%;padding-left:13px;" class="button-big-icon-colse button-white">银行帐号：<span class="float-right"><?= '***' . substr($oneBank->account, -4) ?></span></a>
     <a href="#" style="width:100%;padding-left:13px;" class="button-big-icon-colse button-white">支行地址：<span class="float-right"><?= $oneBank->branch ?></span></a>
     <a href="#" style="width:100%;padding-left:13px;" class="button-big-icon-colse button-white">真实姓名：<span class="float-right"><?= $oneBank->realname; ?></span></a>
     <a href="#" style="width:100%;padding-left:13px;" class="button-big-icon-colse button-white">可用余额：<span class="float-right"><?= $oneuseraccount->use_money ?> 元</span></a>
 </div>
 <div class="decoration"></div>
+<?php
+$form = ActiveForm::begin([
+            'action' => ['/member/account/tixian'],
+            'method' => 'post',
+        ]);
+?>
 <div class="container">
     <div class="toggle-1" style="background-color: #ffffff;">
         <div class="toggle-content padding10" style="overflow: hidden;display: block;">
@@ -34,3 +40,4 @@ $oneBank = Bankcard::find()->where('user_id=:user_id', [':user_id' => $user_id])
 <div class="container no-bottom container-b">
     <?= Html::submitButton('确定提现', ['class' => 'buttonWrap button button-red contactSubmitButton', 'name' => 'submit-button']) ?>
 </div>
+<?php ActiveForm::end(); ?>
