3db6e07c0c443b76420dbae70b1f3ebf2165c938
Administrator <Administrator@bfyang5130>
2015-12-11 16:00:15 +0800

    微信配置

--- frontend/modules/wechat/config.php
+++ frontend/modules/wechat/config.php
@@ -6,10 +6,6 @@ return[
             'class' => 'app\modules\wechat\components\WechatCheck',
             'APPID' => "wx3b55df6bdee5d3fe",
             'APPSECRET' => "fee6f919b912ee0de565387a9467c77a",
-        ],
-        'smser' => [
-            // 微信支付
-            'class' => 'extensions\wxzuanpay\Wxzuanpay',
         ]
     ],
 ];
