d4edfb6b199687227024cadf73307f8cc7f539b4
Administrator <Administrator@bfyang5130>
2015-12-15 16:22:01 +0800

    微信 过年专题

--- frontend/modules/wechat/components/WechatCheck.php
+++ frontend/modules/wechat/components/WechatCheck.php
@@ -458,8 +458,12 @@ class WechatCheck extends Component {
         $resultStr.=sprintf($textfooterTpl, 0);
         $textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><MsgId>%d</MsgId></xml>";
         $resultStr = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $resultStr, $object->MsgId);
+        $textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><MsgId>%d</MsgId></xml>";
+        $resultStr = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $resultStr, $object->MsgId);
         echo $resultStr;
         \Yii::$app->end();
+        //echo $resultStr;
+        //\Yii::$app->end();
     }
 
 //    /**
