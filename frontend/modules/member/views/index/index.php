46815b8ecc0d91f834c81ed1005d063e5d2d1448
Administrator <Administrator@bfyang5130>
2015-12-17 15:52:55 +0800

    增加优惠券

--- frontend/modules/member/views/index/index.php
+++ frontend/modules/member/views/index/index.php
@@ -34,10 +34,6 @@ $thisuserAccount = Account::find()->where(":user_id=user_id", [':user_id' => $us
 </div>
 <div class="decoration"></div>
 <div class="container no-bottom">
-    <a href="<?= Url::toRoute('/member/account/coupon') ?>"  style="width:100%;" class="button-big-colse button-goldden">优惠券</a>
-</div>
-<div class="decoration"></div>
-<div class="container no-bottom">
     <a href="<?= Url::toRoute('/member/index/sharp') ?>" style="width:100%;" class="button-big-icon-colse icon-sharp button-white">快速分享<em class="go-right-member"></em></a>
     <a href="<?= Url::toRoute('/member/index/userinfo') ?>" style="width:100%;" class="button-big-icon-colse icon-baseinfo button-white">基本信息<em class="go-right-member"></em></a>
     <a href="<?= Url::toRoute('/member/index/bank') ?>" style="width:100%;" class="button-big-icon-colse icon-bankcard button-white">我的银行<em class="go-right-member"></em></a>
@@ -55,3 +51,6 @@ $thisuserAccount = Account::find()->where(":user_id=user_id", [':user_id' => $us
     <a href="<?= Url::toRoute('/member/product/fititem') ?>" style="width:100%;" class="button-big-icon-colse icon-proaddress button-white">处理货物<em class="go-right-member"></em></a>
 </div>
 <div class="decoration"></div>
+<div class="container no-bottom">
+    <a href="<?= Url::toRoute('/member/account/coupon') ?>"  style="width:100%;" class="button-big-colse button-goldden">优惠券</a>
+</div>
