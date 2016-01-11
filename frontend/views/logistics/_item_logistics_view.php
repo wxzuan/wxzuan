ac67a81f124ede44e8dbaa28276ed5ae5a9f5761
qinyangsheng <qinyangsheng@192.168.1.3>
2016-01-11 21:04:47 +0800

    修改一个重新开始的用户遇到的BUG

--- frontend/views/logistics/_item_logistics_view.php
+++ frontend/views/logistics/_item_logistics_view.php
@@ -6,10 +6,10 @@ use yii\helpers\Html;
 <div class="decoration"></div>
 <div class="section-title">
     <h4><?= $model->logis_name ?></h4>
-    <h4>佣金：<?= $model->logis_fee ?> 元<em> 保证金：￥ <?= $model->logis_bail ?> 元</em></h4>
-    <div class="one-half-responsive">
+    <div class="one-half-responsive" style="border-top:none; ">
         <p class="quote-item">
             <img src="<?= $model->user->litpic?$model->user->litpic:'/images/wechat/huodong/product.jpg'; ?>" alt="img">
+            佣金：￥ <span style="font-weight: 600;font-size:15px;color:red;"><?= $model->logis_fee ?></span> 元 保证金：￥ <span style="font-weight: 600;font-size:15px;"><?= $model->logis_bail ?></span> 元<br/>
             出发地点: <?= $model->user_country ?> <?= $model->user_province ?> <?= $model->user_city ?> <?= $model->user_area ?><br/>
             目的地点: <?= $model->logis_country ?> <?= $model->logis_provice ?> <?= $model->logis_city ?> <?= $model->logis_area ?><br/>
         </p>
