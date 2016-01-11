1cdfa09410f87a9ea10c10176f97514b7fcd1cad
Administrator <Administrator@bfyang5130>
2016-01-11 15:42:33 +0800

    增加外部物流处理。会员中心的处理未处理。

--- frontend/views/logistics/_item_logistics_view.php
+++ frontend/views/logistics/_item_logistics_view.php
@@ -11,5 +11,5 @@ use yii\helpers\Html;
         出发地点: <?= $model->user_country ?> <?= $model->user_province ?> <?= $model->user_city ?> <?= $model->user_area ?><br/>
         目的地点: <?= $model->logis_country ?> <?= $model->logis_provice ?> <?= $model->logis_city ?> <?= $model->logis_area ?><br/>
     </p>
-    <strong><?= Html::a('<img src="/images/misc/icons/look.png" width="20" alt="img">', FALSE, ['title' => '查看详情', 'value' => Url::toRoute('/logistics/detail/' . $model->id)]); ?></strong>
+    <strong><?= Html::a('<img src="/images/misc/icons/look.png" width="20" alt="img">', ['title' => '查看详情', 'value' => Url::toRoute('/logistics/detail/' . $model->id)]); ?></strong>
 </div>
