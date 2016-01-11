7ecb11bade9511838247e84b13c89fc4a2c64719
qinyangsheng <qinyangsheng@192.168.1.3>
2016-01-11 21:45:09 +0800

    暂时不弄了，玩maya去

--- frontend/views/logistics/logis_detail.php
+++ frontend/views/logistics/logis_detail.php
@@ -6,24 +6,36 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 ?>
 <?= $this->render('@app/views/layouts/main_header.php', ['icons' => ['product-content' => Url::toRoute('/logistics/publishlogistics'), 'twitter-content' => Url::toRoute('/index')]]); ?>
-<?= $this->render('@app/views/layouts/servicesMenu.php'); ?>
 <div class="content">
-    <div class="container no-bottom">
-        <img class="responsive-image" src="<?= $model->logis_s_img ? $model->logis_s_img : '/images/product_demo.jpg' ?>" alt="img">
-    </div>
-    <div class="section-title">
-        <h4><?= $model->logis_name ?></h4>
-        <h4>佣金：<?= $model->logis_fee ?> <em> 保证金：￥ <?= $model->logis_bail ?> 元</em></h4>
-        <p style="line-height: 25px;margin-bottom: 10px;">
-            出发地点： <?= $model->user_country ?> <?= $model->user_province ?> <?= $model->user_city ?> <?= $model->user_area ?><br/>
-            出货地址：<?= $model->user_address ?><br/>
-            目的地点： <?= $model->logis_country ?> <?= $model->logis_provice ?> <?= $model->logis_city ?> <?= $model->logis_area ?><br/>
-            收货地址：<?= $model->logis_detailaddress ?><br/>
-        <div class="well">
-            <?= $model->logis_description ?>
+    <div class="container">
+        <div class="tabs">
+            <a href="#" class="tab-but tab-but-1-qys tab-active">物流信息</a>
+            <a href="#" class="tab-but tab-but-2-qys">物流进度</a>     
         </div>
-        </p>
-        <strong><?= Html::a('<img src="/images/misc/icons/book.png" width="20" alt="img">', FALSE, ['title' => '信息提示', 'value' => Url::toRoute('/logistics/book/' . $model->id), 'class' => 'showModalButton']); ?></strong>
+        <div class="tab-content tab-content-1-qys" style="overflow: hidden; display: block;">
+            <div class="container no-bottom">
+                <img class="responsive-image" src="<?= $model->logis_s_img ? $model->logis_s_img : '/images/product_demo.jpg' ?>" alt="img">
+            </div>
+            <div class="section-title">
+                <h4><?= $model->logis_name ?></h4>
+                <h4>佣金：<?= round($model->logis_fee, 2) ?> <em> 保证金：￥ <?= round($model->logis_bail, 2) ?> 元</em></h4>
+                <p style="line-height: 25px;margin-bottom: 10px;">
+                    出发地点： <?= $model->user_country ?> <?= $model->user_province ?> <?= $model->user_city ?> <?= $model->user_area ?><br/>
+                    出货地址：<?= $model->user_address ?><br/>
+                    目的地点： <?= $model->logis_country ?> <?= $model->logis_provice ?> <?= $model->logis_city ?> <?= $model->logis_area ?><br/>
+                    收货地址：<?= $model->logis_detailaddress ?><br/>
+                <div class="well">
+                    <?= $model->logis_description ?>
+                </div>
+                </p>
+                <strong><?= Html::a('<img src="/images/misc/icons/book.png" width="20" alt="img">', FALSE, ['title' => '信息提示', 'value' => Url::toRoute('/logistics/book/' . $model->id), 'class' => 'showModalButton']); ?></strong>
+            </div>
+        </div>
+        <div class="tab-content tab-content-2-qys" style="overflow: hidden; display: none;">
+            <p>
+                内容建设中
+            </p>
+        </div>       
     </div>
     <?= $this->render('@app/views/layouts/main_footer.php'); ?>
 </div>
--- frontend/web/js/framework.js
+++ frontend/web/js/framework.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     //Go up
 
-    $('.go-up-footer').click(function() {
+    $('.go-up-footer').click(function () {
         $('body,html').animate({
             scrollTop: 0
         }, 800, 'easeOutExpo');
@@ -13,58 +13,58 @@ $(document).ready(function() {
 
     //Checkboxes
 
-    $('.checkbox-one').click(function() {
+    $('.checkbox-one').click(function () {
         $(this).toggleClass('checkbox-one-checked');
         return false;
     });
-    $('.checkbox-two').click(function() {
+    $('.checkbox-two').click(function () {
         $(this).toggleClass('checkbox-two-checked');
         return false;
     });
-    $('.checkbox-three').click(function() {
+    $('.checkbox-three').click(function () {
         $(this).toggleClass('checkbox-three-checked');
         return false;
     });
-    $('.radio-one').click(function() {
+    $('.radio-one').click(function () {
         $(this).toggleClass('radio-one-checked');
         return false;
     });
-    $('.radio-two').click(function() {
+    $('.radio-two').click(function () {
         $(this).toggleClass('radio-two-checked');
         return false;
     });
 
     //Notifications
 
-    $('.tap-dismiss-notification').click(function() {
+    $('.tap-dismiss-notification').click(function () {
         $(this).fadeOut();
         return false;
     });
 
-    $('.close-big-notification').click(function() {
+    $('.close-big-notification').click(function () {
         $(this).parent().fadeOut();
         return false;
     });
 
     //Tabs 
 
-    $('.tab-but-1').click(function() {
-        //	$('.tab-but').removeClass('tab-active');
-        //	$('.tab-but-1').addClass('tab-active');
-        //	$('.tab-content').hide(100);
-        //	$('.tab-content-1').show(100);	
-        //	return false;	
+    $('.tab-but-1-qys').click(function () {
+        $('.tab-but').removeClass('tab-active');
+        $('.tab-but-1-qys').addClass('tab-active');
+        $('.tab-content').hide(100);
+        $('.tab-content-1-qys').show(100);
+        return false;
     });
 
-    $('.tab-but-2').click(function() {
-        //	$('.tab-but').removeClass('tab-active');
-        //	$('.tab-but-2').addClass('tab-active');
-        //	$('.tab-content').hide(100);
-        //	$('.tab-content-2').show(100);
-        //	return false;		
+    $('.tab-but-2-qys').click(function () {
+        $('.tab-but').removeClass('tab-active');
+        $('.tab-but-2-qys').addClass('tab-active');
+        $('.tab-content').hide(100);
+        $('.tab-content-2-qys').show(100);
+        return false;
     });
 
-    $('.tab-but-3').click(function() {
+    $('.tab-but-3').click(function () {
         //	$('.tab-but').removeClass('tab-active');
         //	$('.tab-but-3').addClass('tab-active');
         //	$('.tab-content').hide(100);
@@ -72,7 +72,7 @@ $(document).ready(function() {
         //	return false;	
     });
 
-    $('.tab-but-4').click(function() {
+    $('.tab-but-4').click(function () {
         //	$('.tab-but').removeClass('tab-active');
         //	$('.tab-but-4').addClass('tab-active');
         //	$('.tab-content').hide(100);
@@ -80,7 +80,7 @@ $(document).ready(function() {
         //	return false;		
     });
 
-    $('.tab-but-5').click(function() {
+    $('.tab-but-5').click(function () {
         $('.tab-but').removeClass('tab-active');
         $('.tab-but-5').addClass('tab-active');
         $('.tab-content').hide(100);
@@ -92,13 +92,13 @@ $(document).ready(function() {
 
 
 
-    $('.deploy-toggle-2').click(function() {
+    $('.deploy-toggle-2').click(function () {
         $(this).parent().find('.toggle-content').toggle(100);
         $(this).toggleClass('toggle-2-active');
         return false;
     });
 
-    $('.deploy-toggle-3').click(function() {
+    $('.deploy-toggle-3').click(function () {
         $(this).parent().find('.toggle-content').toggle(100);
         $(this).find('em strong').toggleClass('toggle-3-active-ball');
         $(this).find('em').toggleClass('toggle-3-active-background');
@@ -107,7 +107,7 @@ $(document).ready(function() {
 
     //Submenu Nav
 
-    $('.submenu-nav-deploy').click(function() {
+    $('.submenu-nav-deploy').click(function () {
         $(this).toggleClass('submenu-nav-deploy-active');
         $(this).parent().find('.submenu-nav-items').toggle(100);
         return false;
@@ -115,14 +115,14 @@ $(document).ready(function() {
 
     //Sliding Door
 
-    $('.sliding-door-top').click(function() {
+    $('.sliding-door-top').click(function () {
         $(this).animate({
             left: '101%'
         }, 500, 'easeInOutExpo');
         return false;
     });
 
-    $('.sliding-door-bottom a em').click(function() {
+    $('.sliding-door-bottom a em').click(function () {
         $(this).parent().parent().parent().find('.sliding-door-top').animate({
             left: '0px'
         }, 500, 'easeOutBounce');
