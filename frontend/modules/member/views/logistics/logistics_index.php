b81f0ee43c75edd2749fe02be3f641fb78aaf3db
Administrator <Administrator@bfyang5130>
2016-01-14 11:19:05 +0800

    物流处理一堆坑没填

--- frontend/modules/member/views/logistics/logistics_index.php
+++ frontend/modules/member/views/logistics/logistics_index.php
@@ -86,23 +86,4 @@ $logisLists = LogisticsService::findMyLogiss($data);
         </div>
     <?php endif; ?>
 </div>
-<div class="container no-bottom" style="padding:0px 10px;">
-    <?php
-    $user_id = Yii::$app->user->getId();
-    $data = ['user_id' => $user_id, 'limit' => 5];
-    $logisLists = LogisticsService::findMyLogiss($data);
-    if ($logisLists):
-        echo ListView::widget([
-            'dataProvider' => $logisLists,
-            'itemOptions' => ['class' => 'item'],
-            'itemView' => '_item_logis_index_view',
-            'pager' => ['class' => ScrollPager::className()]
-        ]);
-    else:
-        ?>
-        <div class="container" style="min-height: 350px;">
-            <p>暂时没有物品记录</p>
-        </div>
-    <?php endif; ?>
-</div>
 <div class="decoration"></div>
