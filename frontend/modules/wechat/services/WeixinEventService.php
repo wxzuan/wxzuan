8b04e2c98966f7b6ed8bb713b9a1b091307c32a4
qinyangsheng <qinyangsheng@192.168.1.3>
2016-01-21 21:32:13 +0800

    测试收货流程

--- frontend/modules/wechat/services/WeixinEventService.php
+++ frontend/modules/wechat/services/WeixinEventService.php
@@ -54,13 +54,13 @@ class WeixinEventService {
     public static function fitScancode_waitmsg($object, User $weixinuser) {
         $scanType = $object->ScanCodeInfo->ScanType;
         $content = " 扫描结果：" . $object->ScanCodeInfo->ScanResult;
-//        if ($scanType == 'qrcode') {
-//            $string = $object->ScanCodeInfo->ScanResult;
-//            #解码
-//            #获得ID
-//            $data = explode(",", $string);
-//            if (is_numeric($data[0]) && isset($data[1]) && is_numeric($data[1]) && isset($data[2]) && !empty($data[2])) {
-//                #获得货物数据
+        if ($scanType == 'qrcode') {
+            $string = $object->ScanCodeInfo->ScanResult;
+            #解码
+            #获得ID
+            $data = explode(",", $string);
+            if (is_numeric($data[0]) && isset($data[1]) && is_numeric($data[1]) && isset($data[2]) && !empty($data[2])) {
+                #获得货物数据
 //                switch ($data[0]) {
 //                    case 0:
 //                        $content = LogisticsService::fitOutCode($weixinuser, $data);
@@ -70,8 +70,9 @@ class WeixinEventService {
 //                        break;
 //                    default :$content = '该信息不存在或者已经被接单。';
 //                }
-//            }
-//        }
+                $content=$data[0];
+            }
+        }
         WechatCheck::_transmitText($object, $content);
     }
 
