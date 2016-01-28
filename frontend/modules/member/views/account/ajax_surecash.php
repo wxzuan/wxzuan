aa78f3cd7589845da9afac834b4f1112907eae2a
Administrator <Administrator@bfyang5130>
2016-01-28 17:55:20 +0800

    增加取消提现功能

--- frontend/modules/member/views/account/ajax_surecash.php
+++ frontend/modules/member/views/account/ajax_surecash.php
@@ -10,7 +10,7 @@ if (!isset($p_param['sure'])):
         您确定要给取消该笔提现吗？
     </p>
     <div class="modal-footer">
-        <?= Html::a('确认取消', FALSE, ['title' => '信息提示', 'value' => Url::toRoute('/member/product/cancelcash/' . $order->id) . '?sure=1', 'class' => 'btn btn-warning showModalButton']); ?>
+        <?= Html::a('确认取消', FALSE, ['title' => '信息提示', 'value' => Url::toRoute('/member/account/cancelcash/' . $order->id) . '?sure=1', 'class' => 'btn btn-warning showModalButton']); ?>
         <button type="button" class="btn btn-danger" data-dismiss="modal">关闭</button>
     </div>
     <?php
