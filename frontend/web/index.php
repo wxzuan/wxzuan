5c078626a99c3672e4a3033613d650842d8157da
qinyangsheng <qinyangsheng@192.168.1.3>
2016-02-19 00:34:29 +0800

    修改最小化配置

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,6 +1,7 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV_DEV') or define('YII_ENV_DEV', 'dev');
+#defined('YII_ENV_DEV') or define('YII_ENV_DEV', 'dev');
+defined('YII_ENV_PRO') or define('YII_ENV_PRO', 'pro');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
