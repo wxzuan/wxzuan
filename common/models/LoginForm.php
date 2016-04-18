6821aecf045142bcbd74c510d3830a135303638e
qinyangsheng <qinyangsheng@192.168.1.102>
2016-04-19 01:33:00 +0800

    吐槽实际私密功能

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -75,6 +75,7 @@ class LoginForm extends Model {
         if ($this->validate()) {
             return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         } else {
+            print_r($this->errors);exit;
             return false;
         }
     }
