dbd17a750b32c565384ce6b1bfa9ecf905389ff2
qinyangsheng <qinyangsheng@192.168.1.102>
2016-04-22 00:17:17 +0800

    增加装修功能

--- frontend/views/layouts/servicesMenu.php
+++ frontend/views/layouts/servicesMenu.php
@@ -10,6 +10,7 @@ $thisactiveid = strtolower(\Yii::$app->controller->id);
             <a href="<?= Url::toRoute('/product/index') ?>" class="tab-but tab-but-1<?= ($thisactiveid == 'product') ? ' tab-active' : ''; ?>">商品区</a>
             <a href="<?= Url::toRoute('/logistics/index') ?>" class="tab-but tab-but-2<?= ($thisactiveid == 'logistics') ? ' tab-active' : ''; ?>">物运区</a>
             <a href="<?= Url::toRoute('/product/index') ?>" class="tab-but tab-but-3">理财区</a>
+            <a href="<?= Url::toRoute('/fixture/index') ?>" class="tab-but tab-but-5<?= ($thisactiveid == 'fixture') ? ' tab-active' : ''; ?>">装修区</a>
             <a href="<?= Url::toRoute('/say/index') ?>" class="tab-but tab-but-4<?= ($thisactiveid == 'say') ? ' tab-active' : ''; ?>">吐槽区</a>     
         </div>     
     </div>
